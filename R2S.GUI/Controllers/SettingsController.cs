128c2806bf3dbab593e58b969f4310b10488f860
Ouerghi Yassine <yassine.ouerghi@esprit.tn>
2016-11-20 20:46:09 +0100

    candidate form

--- R2S.GUI/Controllers/SettingsController.cs
+++ R2S.GUI/Controllers/SettingsController.cs
@@ -13,45 +13,6 @@ namespace R2S.GUI.Controllers
 {
     public class SettingsController : BaseController
     {
-        private IJobFieldService _jobFieldService =  new JobFieldService();
-
-        // GET: Settings
-        [HttpGet]
-        public ActionResult JobForm()
-        {
-            return View(new JobFieldViewModel() { User = CurrentUser });
-        }
-
-        [HttpPost]
-        public ActionResult Add()
-        {
-            string postData = new System.IO.StreamReader(Request.InputStream).ReadToEnd();
-            string data = HttpUtility.UrlDecode(postData);
-            JObject json = JObject.Parse(data);
-
-            var text = json.GetValue("text");
-            var radio = json.GetValue("radio");
-            var checkbox = json.GetValue("checkox");
-
-            foreach (var c in checkbox.Values())
-            {
-                jobfield field = new jobfield() { fieldName = c.ToString(), fieldType = jobfield.Checkbox };
-                _jobFieldService.Add(field);
-            }
-            foreach (var c in text.Values())
-            {
-                jobfield field = new jobfield() { fieldName = c.ToString(), fieldType = jobfield.TextField };
-                _jobFieldService.Add(field);
-            }
-            foreach (var c in radio.Values())
-            {
-                jobfield field = new jobfield() { fieldName = c.ToString(), fieldType = jobfield.Radiobox };
-                _jobFieldService.Add(field);
-            }
-
-            _jobFieldService.commit();
-
-            return Json("done");
-        }
+        
     }
 }
---
+++ R2S.Service/CandidateFieldService.cs
@@ -0,0 +1,18 @@
+﻿using R2S.Data.Infrastructures;
+using R2S.Data.Models;
+using Service.pattern;
+
+namespace R2S.Service
+{
+    public class CandidateFieldService : ServiceGenerique<candidatefield>, ICandidateFieldService
+    {
+        private static IDataBaseFactory dbfactory = new DataBaseFactory();
+        private static UnitOfWork Utw = new UnitOfWork(dbfactory);
+
+        public CandidateFieldService() : base(Utw)
+        {
+
+        }
+
+    }
+}
