a714ee130c836065c509c380712156e2daa84950
Oueghi Yassine <yassine.ouerghi@esprit.tn>
2016-11-22 11:33:28 +0100

    Skills Managment

---
+++ R2S.Service/Interfaces/ISkillService.cs
@@ -0,0 +1,14 @@
+﻿using R2S.Data.Models;
+using Service.pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2S.Service.Interfaces
+{
+    public interface ISkillService : IServiceGenerique<skill>
+    {
+    }
+}
---
+++ R2S.Service/SkillService.cs
@@ -0,0 +1,23 @@
+﻿using R2S.Data.Infrastructures;
+using R2S.Data.Models;
+using R2S.Service.Interfaces;
+using Service.pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2S.Service
+{
+    public class SkillService : ServiceGenerique<skill>, ISkillService
+    {
+        private static IDataBaseFactory dbfactory = new DataBaseFactory();
+        private static UnitOfWork Utw = new UnitOfWork(dbfactory);
+
+        public SkillService() : base(Utw)
+        {
+
+        }
+    }
+}
